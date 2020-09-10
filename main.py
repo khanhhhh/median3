5267adcf8d2b230d19474a779a4c526738e883e4
khanhhhh <nguyenngockhanh.pbc@gmail.com>
2020-09-11 00:55:39 +0800

    init

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 
 import numpy as np
+from scipy import ndimage
 
 from median import median3_filter
 
@@ -8,5 +9,11 @@ im = np.random.random(size= (3, 800, 600)).astype(np.float32)
 
 t0 = time.time()
 median3_filter(im)
+t1 = time.time()
+print(t1-t0)
+
+
+t0 = time.time()
+ndimage.median_filter(im, size= 3)
 t1 = time.time()
 print(t1-t0)
